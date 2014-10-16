ff09ade8fbd4ca9cd5206feae3ad65a725f0b822
Eric Muyser <eric@muyser.com>
2014-10-16 10:14:02 -0700

    Track all the things
        * Reviews checked
        * Sidebar opened/closed
        * Accordion first clicked (item ID)
        * Accordion viewed entirely
        * Accordion items opened more than one at a time or not
        * Add to bag (success and failure)
        * Mini-Cart editing enabled/disabled
        * Mini-Cart item quantity changed
        * Mini-Cart item removed
        * PLP filters toggled

--- src/wiretap.js
+++ src/wiretap.js
@@ -30,12 +30,20 @@
     * Color adjustments
     * Quantity adjustments
     * Size adjustments
+    * Reviews checked
+    * Sidebar opened/closed
+    * Accordion first clicked (item ID)
+    * Accordion viewed entirely
+    * Accordion items opened more than one at a time or not
+    * Add to bag (success and failure)
+    * Mini-Cart editing enabled/disabled
+    * Mini-Cart item quantity changed
+    * Mini-Cart item removed
+    * PLP filters toggled
 
     Tracking TODO:
-    * Add to bag (success and failure)
     * PLP filter interactions
-    * Minicart quantity change
-    * Whether bellows is being opened one at a time or multiple without closing previous
+    * Changing to different products on Multi-PDP
 */
 define([
     '$'
@@ -43,8 +51,47 @@ define([
     function($) {
         var Wiretap = {};
         var carousels = {};
+        var accordions = {};
         var swiping = false;
-        var fullCarouselViewEventFired = false;
+
+        // Wiretap.setup
+        // Initializes Wiretap and sets up implicitly tracked events: Wiretap.orientationChange, Wiretap.scrollToBottom.
+        Wiretap.setup = function() {
+            // Bind events
+            $(window).on('touchmove', function() {
+                swiping = true;
+            });
+
+            $(window).on('touchend', function() {
+                window.setTimeout(function() {
+                    swiping = false;
+                }, 50);
+            });
+
+            $(window).on('orientationchange', function() {
+                Wiretap.orientationChange();
+            });
+
+            $(window).scroll(function() {
+                if ($(window).scrollTop() + $(window).height() === $(document).height()) {
+                    Wiretap.scrollToBottom();
+                }
+            });
+        };
+
+        // Wiretap.proxyClassicAnalytics
+        // Proxies the classic Google Analytics call so that we capture events fired by desktop
+        // We then send them through Mobify's analytics call
+        // Example: _gaq.push(["_trackEvent", "product selection", "select a size", a(this.options[this.selectedIndex]).text().trim()])
+        Wiretap.proxyClassicAnalytics = function() {
+            var originalPush = window._gaq.push;
+
+            window._gaq.push = function(data) {
+                Mobify.analytics.ua('mobifyTracker.send', 'event', 'Desktop Event: ' + data[1], data[2], data[3], {'nonInteraction': 1});
+
+                return originalPush(data);
+            };
+        };
 
         // Wiretap.orientationChange
         // eg. Wiretap.orientationChange();
@@ -59,7 +106,7 @@ define([
 
         // Wiretap.carouselSwipe
         // title = Home, PDP, Related Images
-        // slide = 1, 2, 3, 4, 5, 6
+        // currentSlide = 1, 2, 3, 4, 5, 6
         // eg. Wiretap.carouselSwipe('PDP', 1);
         Wiretap.carouselSwipe = function(title, currentSlide) {
             var currentCarousel = carousels[title];
@@ -71,8 +118,8 @@ define([
             currentCarousel.swipes.push(currentSlide);
 
             // If the user has swiped as much as there swipes, maybe they've been to every slide?
-            if (!fullCarouselViewEventFired && currentCarousel.swipes.length >= currentCarousel.totalSlides) {
-                var fullCarouselView = true;
+            if (!currentCarousel.fullViewFired && currentCarousel.swipes.length >= currentCarousel.totalSlides) {
+                currentCarousel.fullView = true;
 
                 // Iterate the amount of slides,
                 // By default lets say they have seen all slides
@@ -80,22 +127,17 @@ define([
                 // Rather than firing the event
                 for (var i = 0, l = currentCarousel.totalSlides; i <= l; ++i) {
                     if (!currentCarousel.swipes.indexOf(i + 1)) {
-                        fullCarouselView = false;
+                        currentCarousel.fullView = false;
                         break;
                     }
                 }
 
-                if (fullCarouselView) {
+                if (currentCarousel.fullView) {
                     Mobify.analytics.ua('mobifyTracker.send', 'event', 'Carousel - ' + title, 'completeView', currentSlide + '');
 
-                    fullCarouselViewEventFired = true;
+                    currentCarousel.fullViewFired = true;
                 }
             }
-        };
-
-        // Wiretap.carouselInteraction
-        Wiretap.carouselInteraction = function(title, currentSlide) {
-            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Carousel - ' + title, 'interaction', currentSlide + '');
         };
 
         // Wiretap.carouselLoad
@@ -103,6 +145,8 @@ define([
             // If the carousel hasn't been initialized, set it up
             if (!carousels.hasOwnProperty(title)) {
                 carousels[title] = {
+                    fullView: false,
+                    fullViewFired: false,
                     totalSlides: totalSlides,
                     swipes: []
                 };
@@ -202,27 +246,94 @@ define([
             Mobify.analytics.ua('mobifyTracker.send', 'event', title, 'Check Reviews');
         };
 
+        Wiretap.sidebarOpened = function(title) {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', title, 'Sidebar Opened');
+        };
 
-        // Bind events
-        $(window).on('touchmove', function() {
-            swiping = true;
-        });
+        Wiretap.sidebarClosed = function(title) {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', title, 'Sidebar Closed');
+        };
 
-        $(window).on('touchend', function() {
-            window.setTimeout(function() {
-                swiping = false;
-            }, 50);
-        });
+        Wiretap.cartItemAdded = function() {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Cart', 'item-added', 'OK');
+        };
 
-        $(window).on('orientationchange', function() {
-            Wiretap.orientationChange();
-        });
+        Wiretap.minicartRemoveItem = function() {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Mini-Cart', 'item-removed', 'OK');
+        };
 
-        $(window).scroll(function() {
-            if ($(window).scrollTop() + $(window).height() === $(document).height()) {
-                Wiretap.scrollToBottom();
+        Wiretap.minicartEditEnable = function() {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Mini-Cart', 'edit-enabled', 'OK');
+        };
+
+        Wiretap.minicartEditDisable = function() {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Mini-Cart', 'edit-disabled', 'OK');
+        };
+
+        Wiretap.minicartChangeQuantity = function() {
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Mini-Cart', 'quantity-changed', 'OK');
+        };
+
+        Wiretap.accordionOpen = function(title, currentItem) {
+            var currentAccordion = accordions[title];
+
+            if (!currentAccordion.opens.length) {
+                Mobify.analytics.ua('mobifyTracker.send', 'event', 'Accordion - ' + title, 'first-open', currentItem + '');
             }
-        });
+
+            currentAccordion.opens.push(currentItem);
+
+            // If there's more than one accordion item opened, and we've opened more than we've closed
+            // Then this user doesn't mind having multiple opened
+            // Send how many are currently opened and haven't been closed
+            if (currentAccordion.opens.length > 1 && currentAccordion.opens.length > currentAccordion.closes.length) {
+                Mobify.analytics.ua('mobifyTracker.send', 'event', 'Accordion - ' + title, 'multipleOpened', currentAccordion.opens.length - currentAccordion.closes.length);
+            }
+
+            // If the user has swiped as much as there swipes, maybe they've been to every slide?
+            if (!currentAccordion.fullViewFired && currentAccordion.opens.length >= currentAccordion.totalItems) {
+                currentAccordion.fullView = true;
+
+                // Iterate the amount of slides,
+                // By default lets say they have seen all slides
+                // But if we find a slide we haven't visited, we bail
+                // Rather than firing the event
+                for (var i = 0, l = currentAccordion.totalItems; i <= l; ++i) {
+                    if (!currentAccordion.opens.indexOf(i + 1)) {
+                        currentAccordion.fullView = false;
+                        break;
+                    }
+                }
+
+                if (currentAccordion.fullView) {
+                    Mobify.analytics.ua('mobifyTracker.send', 'event', 'Accordion - ' + title, 'completeView', currentItem + '');
+
+                    currentAccordion.fullViewFired = true;
+                }
+            }
+        };
+
+        Wiretap.accordionClose = function(title, currentItem) {
+            var currentAccordion = accordions[title];
+
+            currentAccordion.closes.push(currentItem);
+        };
+
+        // Wiretap.accordionLoad
+        Wiretap.accordionLoad = function(title, totalItems) {
+            // If the accordion hasn't been initialized, set it up
+            if (!accordions.hasOwnProperty(title)) {
+                accordions[title] = {
+                    fullView: false,
+                    fullViewFired: false,
+                    totalItems: totalItems,
+                    opens: [],
+                    closes: []
+                };
+            }
+
+            Mobify.analytics.ua('mobifyTracker.send', 'event', 'Accordion - ' + title, 'load', totalItems + '', {'nonInteraction': 1});
+        };
 
         return Wiretap;
     });
