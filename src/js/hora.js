1a8b6241a406c2dbbc9c914b1141186dab929c3d
Eric Muyser <eric@muyser.com>
2014-11-27 14:51:26 -0800

    Remove features commenting

--- src/js/hora.js
+++ src/js/hora.js
@@ -3,47 +3,8 @@
 /*
  Hora.js
  -------
- A library that assists with tracking user-behavior with custom Google Analytics events
+ Assists with tracking user-behavior with custom Google Analytics events
 
- Implicit tracking:
- * Re-orientation (landscape to portrait and visa-versa)
- * Page fully scrolled to bottom
-
- Explicit tracking (requires development):
- * Carousel first swipe (slide ID)
- * Carousel viewed entirely
- * Carousel slide clicked
- * Carousel arrow control clicked
- * Carousel loaded
- * Carousel slide zoomed
- * Sidebar navigation item clicked (up to 3 levels deep)
- * Header search toggled
- * Header minicart toggled
- * Breadcrumb interacted with
- * Back to top clicked
- * Newsletter interacted with
- * Footer interacted with
- * Pagination interacted with
- * Size guide opened
- * Email to friend opened
- * Email me back opened
- * Color adjustments
- * Quantity adjustments
- * Size adjustments
- * Reviews checked
- * Sidebar opened/closed
- * Accordion first clicked (item ID)
- * Accordion viewed entirely
- * Accordion items opened more than one at a time or not
- * Add to bag (success and failure)
- * Mini-Cart editing enabled/disabled
- * Mini-Cart item quantity changed
- * Mini-Cart item removed
- * PLP filters toggled
-
- Tracking TODO:
- * PLP filter interactions
- * Changing to different products on Multi-PDP
  */
 define([
         '$'
