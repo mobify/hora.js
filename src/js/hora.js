39b3d32629f480635aa0c9de42ea9c4f5d429280
Helen <helen@mobify.com>
2015-04-07 10:21:22 -0700

    rack specific horajs

--- src/js/hora.js
+++ src/js/hora.js
@@ -85,7 +85,7 @@ define([
             args.unshift('mobifyTracker.send', 'event');
 
             if (Hora.isDebug) {
-                console.log('Parameters: %O', args);
+                console.log('%cHora Parameters: %O', 'background: #222; color: #bada55', args);
             } else {
                 Mobify.analytics.ua.apply(null, args);
             }
@@ -105,17 +105,7 @@ define([
                         _swiping = false;
                     }, 50);
                 })
-                .on('orientationchange', Hora.orientation.change)
-                .on('load', function() {
-                    var windowHeight = $window.height();
-
-                    $window
-                        .on('scroll', function() {
-                            if ($window.scrollTop() + windowHeight === $doc.height()) {
-                                Hora.scroll.bottom('Page');
-                            }
-                        });
-                });
+                .on('orientationchange', Hora.orientation.change);
 
             (function patchAlerts() {
                 var _alert = window.alert;
@@ -221,48 +211,10 @@ define([
                 title = 'Carousel - ' + title;
 
                 if (_swiping) {
-                    // metric15 - First Item
-                    //  1 - was the first item swiped
-                    //  0 - was not the first item swiped
-                    Hora.send(title, 'Swipe', 'Slide #' + currentSlide, currentSlide, {
-                        'metric15': currentCarousel.swipes.length === 0 ? 1 : 0
-                    });
-
-                    currentCarousel.swipes.push(currentSlide);
+                    Hora.send(title, 'Swipe', 'Slide #' + currentSlide);
                 }
                 else {
-                    // metric15 - First Item
-                    //  1 - was the first item moved
-                    //  0 - was not the first item moved
-                    Hora.send(title, 'Move', 'Slide #' + currentSlide, currentSlide, {
-                        'metric15': currentCarousel.slides.length === 0 ? 1 : 0
-                    });
-
-                    currentCarousel.slides.push(currentSlide);
-                }
-
-                currentCarousel.viewed.push(currentSlide);
-
-                // If the user has swiped as much as there swipes, maybe they've been to every slide?
-                if (!currentCarousel.fullViewFired && currentCarousel.viewed.length >= currentCarousel.totalSlides) {
-                    currentCarousel.fullView = true;
-
-                    // Iterate the amount of slides,
-                    // By default lets say they have seen all slides
-                    // But if we find a slide we haven't visited, we bail
-                    // Rather than firing the event
-                    for (var i = 1, l = currentCarousel.totalSlides; i <= l; ++i) {
-                        if (currentCarousel.viewed.indexOf(i) === -1) {
-                            currentCarousel.fullView = false;
-                            break;
-                        }
-                    }
-
-                    if (currentCarousel.fullView) {
-                        Hora.send(title, 'View All Slides', 'Total ' + currentCarousel.totalSlides, currentCarousel.totalSlides);
-
-                        currentCarousel.fullViewFired = true;
-                    }
+                    Hora.send(title, 'Move', 'Slide #' + currentSlide);
                 }
             },
 
@@ -293,147 +245,7 @@ define([
 
                 title = 'Carousel - ' + title;
 
-                // metric15 - First Item
-                //  1 - was the first item zoomed
-                //  0 - was not the first item zoomed
-                Hora.send(title, 'Zoom', 'Slide #' + currentSlide, currentSlide, {
-                    'metric15': currentCarousel.zooms.length === 0 ? 1 : 0
-                });
-
-                currentCarousel.zooms.push(currentSlide);
-            },
-
-            slideClick: function(title, currentSlide) {
-                var currentCarousel = _carousels[title];
-
-                title = 'Carousel - ' + title;
-
-                // metric15 - First Item
-                //  1 - was the first item clicked
-                //  0 - was not the first item clicked
-                Hora.send(title, 'Click',  'Slide #' + currentSlide, currentSlide, {
-                    'metric15': currentCarousel.clicks.length === 0 ? 1 : 0
-                });
-
-                currentCarousel.clicks.push(currentSlide);
-            },
-
-            iconClick: function(title, currentSlide, direction) {
-                var currentCarousel = _carousels[title];
-                var directionTitle = (direction === -1) ? 'Previous' : 'Next';
-
-                title = 'Carousel - ' + title;
-
-                // metric15 - First Item
-                //  1 - was the first item icon clicked
-                //  0 - was not the first item icon clicked
-                Hora.send(title, directionTitle + ' Icon', 'Slide #' + currentSlide, currentSlide, {
-                    'metric15': currentCarousel.icons.length === 0 ? 1 : 0
-                });
-
-                currentCarousel.icons.push(currentSlide);
-            }
-        };
-
-        Hora.navigation = {
-            click: function(menuTitle, itemTitle) {
-                Hora.send('Navigation - ' + menuTitle, 'Click', itemTitle);
-            }
-        };
-
-        Hora.search = {
-            toggle: function() {
-                Hora.send('Search', 'Toggle');
-            }
-        };
-
-        Hora.breadcrumb = {
-            click: function() {
-                Hora.send('Breadcrumb', 'Click');
-            }
-        };
-
-        Hora.backToTop = {
-            click: function() {
-                Hora.send('Back To Top', 'Click');
-            }
-        };
-
-        Hora.newsletter = {
-            click: function() {
-                Hora.send('Newsletter', 'Click');
-            }
-        };
-
-        Hora.footer = {
-            click: function() {
-                Hora.send('Footer', 'Click');
-            }
-        };
-
-        Hora.pagination = {
-            click: function() {
-                Hora.send('Pagination', 'Click');
-            }
-        };
-
-        Hora.filter = {
-            toggle: function(title) {
-                Hora.send('Filter - ' + title, 'Toggle');
-            },
-            change: function(title, type, amount) {
-                Hora.send('Filter - ' + title, 'Change: ' + type, amount);
-            }
-        };
-
-        Hora.scroll = {
-            up: function(title) {
-                Hora.send('Scroll - ' + title, 'Up');
-            },
-            down: function(title) {
-                Hora.send('Scroll - ' + title, 'Down');
-            },
-            top: function(title) {
-                Hora.send('Scroll - ' + title, 'Top');
-            },
-            bottom: function(title) {
-                Hora.send('Scroll - ' + title, 'Bottom');
-            }
-        };
-
-        Hora.sizeGuide = {
-            open: function(title) {
-                Hora.send('Size Guide - ' + title, 'Open');
-            }
-        };
-
-        Hora.emailFriend = {
-            open: function(title) {
-                Hora.send('Email Friend - ' + title, 'Open');
-            }
-        };
-
-        Hora.emailMeBack = {
-            open: function(title) {
-                Hora.send('Email Me Back - ' + title, 'Open');
-            }
-        };
-
-        Hora.color = {
-            change: function(title) {
-                Hora.send('Color - ' + title, 'Change');
-            }
-        };
-
-        Hora.quantity = {
-            change: function(title, amount) {
-                Hora.send('Quantity - ' + title, 'Change', null, amount);
-            }
-        };
-
-        Hora.size = {
-            change: function(title, amount) {
-                Hora.send('Size - ' + title, 'Change', null, amount);
+                Hora.send(title, 'Zoom', 'Slide #' + currentSlide);
             }
         };
 
@@ -455,116 +267,9 @@ define([
             }
         };
 
-        Hora.review = {
-            read: function(title) {
-                Hora.send('Review - ' + title, 'Read');
-            }
-        };
-
-        Hora.sidebar = {
-            open: function(title) {
-                Hora.send('Sidebar - ' + title, 'Open');
-            },
-            close: function(title) {
-                Hora.send('Sidebar - ' + title, 'Close');
-            }
-        };
-
-        Hora.cart = {
-            addItem: function() {
-                var fullCarouselView = false;
-
-                for (var title in _carousels) {
-                    if (_carousels.hasOwnProperty(title)) {
-                        var carousel = _carousels[title];
-
-                        if (carousel.fullViewFired) {
-                            fullCarouselView = true;
-                        }
-                    }
-                }
-
-                // metric15 - View All Carousel Slides
-                //  1 - viewed all carousel slides
-                //  0 - didn't view all carousel slides
-                Hora.send('Cart', 'Add Item', 'None', 0, {
-                    'metric15': fullCarouselView ? 1 : 0
-                });
-            },
-            removeItem: function() {
-                Hora.send('Cart', 'Remove Item', 'None', 0);
-            }
-        };
-
-        Hora.minicart = {
-            toggle: function() {
-                Hora.send('Mini-Cart', 'Toggle');
-            },
-
-            enableEdit: function() {
-                Hora.send('Mini-Cart', 'Enable Edit');
-            },
-
-            disableEdit: function() {
-                Hora.send('Mini-Cart', 'Disable Edit');
-            },
-
-            changeQuantity: function(amount) {
-                Hora.quantity.change('Mini-Cart', amount);
-            }
-        };
-
-        Hora.checkout = {
-            start: function(message) {
-                Hora.send('Checkout', 'Start', message);
-            }
-        };
-
         Hora.accordion = {
             open: function(title, currentItem) {
-                var currentAccordion = _accordions[title];
-
-                title = 'Accordion - ' + title;
-
-                // metric15 - First Item
-                //  1 - was the first item opened
-                //  0 - was not the first item opened
-                Hora.send(title, 'Open', 'Item #' + currentItem, currentItem, {
-                    'metric15': currentAccordion.opens.length === 0 ? 1 : 0
-                });
-
-                currentAccordion.opens.push(currentItem);
-
-                // If there's more than one accordion item opened, and we've opened more than we've closed
-                // Then this user doesn't mind having multiple opened
-                // Send how many are currently opened and haven't been closed
-                if (currentAccordion.opens.length > 1 && currentAccordion.opens.length > currentAccordion.closes.length) {
-                    var total = currentAccordion.opens.length - currentAccordion.closes.length;
-
-                    Hora.send(title, 'Open Multiple Items', 'Total ' + total, total);
-                }
-
-                // If the user has swiped as much as there swipes, maybe they've been to every slide?
-                if (!currentAccordion.fullViewFired && currentAccordion.opens.length >= currentAccordion.totalItems) {
-                    currentAccordion.fullView = true;
-
-                    // Iterate the amount of slides,
-                    // By default lets say they have seen all slides
-                    // But if we find a slide we haven't visited, we bail
-                    // Rather than firing the event
-                    for (var i = 1, l = currentAccordion.totalItems; i <= l; ++i) {
-                        if (currentAccordion.opens.indexOf(i) === -1) {
-                            currentAccordion.fullView = false;
-                            break;
-                        }
-                    }
-
-                    if (currentAccordion.fullView) {
-                        Hora.send(title, 'View All Items', 'Total ' + currentAccordion.totalItems, currentAccordion.totalItems);
-
-                        currentAccordion.fullViewFired = true;
-                    }
-                }
+                Hora.send('Accordion - ' + title, 'Open', currentItem);
             },
 
             close: function(title, currentItem) {
@@ -588,6 +293,25 @@ define([
                 Hora.send('Accordion - ' + title, 'Load', 'Total ' + totalItems, totalItems, NON_INTERACTION);
             }
         };
+
+        Hora.pinny = {
+            open: function (title) {
+                Hora.send('Pinny', 'Open', title);
+            },
+
+            close: function (title) {
+                Hora.send('Pinny', 'Close', title);
+            },
+            trigger: function (category, label) {
+                Hora.send(category + ' Pinny', 'Trigger By', label);
+            }
+        }
+
+        Hora.button = {
+            click: function (title) {
+                Hora.send('Button', 'Click', title);
+            }
+        }
 
         return Hora;
     });
